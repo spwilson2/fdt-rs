6443d79f20d43c668132b32b066cd0164678ea46
Sean Wilson <sean.wilson@ghs.com>
2020-06-21 16:01:09 -0700

    Change to raw buffer method for allocation

--- src/index.rs
+++ src/index.rs
@@ -9,11 +9,18 @@
 
 #![allow(dead_code)] // TODO/FIXME
 #![allow(unused_variables)]
+#![allow(unused_imports)]
 use crate::iters::AssociatedOffset;
 use crate::unsafe_unwrap::UnsafeUnwrap;
 use crate::*;
 
 use core::marker::PhantomData;
+use core::mem::{size_of, align_of};
+use core::ptr::{null, null_mut};
+
+unsafe fn ptr_in<T>(buf: &[u8], ptr: *const T) -> bool {
+    buf.as_ptr().add(buf.len()) < (ptr as *const u8)
+}
 
 // TODO Add a wrapper around these that is easier to use (that includes a reference to the fdt).
 pub struct DTIProp<'dt> {
@@ -21,8 +28,8 @@ pub struct DTIProp<'dt> {
     nameoff: AssociatedOffset<'dt>,
 }
 
-impl<'dt> From<iters::ParsedProp<'dt>> for DTIProp<'dt> {
-    fn from(prop: iters::ParsedProp<'dt>) -> Self {
+impl<'dt> From<&iters::ParsedProp<'dt>> for DTIProp<'dt> {
+    fn from(prop: &iters::ParsedProp<'dt>) -> Self {
         Self {
             propbuf: prop.prop_buf,
             nameoff: prop.name_offset,
@@ -33,173 +40,209 @@ impl<'dt> From<iters::ParsedProp<'dt>> for DTIProp<'dt> {
 // TODO Add a wrapper around these that is easier to use (that includes a reference to the fdt).
 struct DTINode<'dt, 'i: 'dt> {
     parent: Option<*const DTINode<'dt, 'i>>,
-    children: Vec<DTINode<'dt, 'i>>,
-    props: Vec<DTIProp<'dt>>,
+    children: &'i [DTINode<'dt, 'i>],
+    props: &'i [DTIProp<'dt>],
     name: &'dt [u8],
-    _index: PhantomData<&'i [u8]>,
 }
 
-impl<'dt, 'i: 'dt> DTINode<'dt, 'i> {
-    fn new(
-        parent: Option<*const DTINode<'dt, 'i>>,
-        node: iters::ParsedBeginNode<'dt>,
-    ) -> Self {
+struct GrowableSlice<'l, T> {
+    ptr: *const T,
+    size: usize,
+    _lt: PhantomData<&'l T>,
+}
+
+impl<'l, T> GrowableSlice<'l, T> {
+    fn new() -> Self {
         Self {
-            parent,
-            children: Vec::new(),
-            props: Vec::new(),
-            name: node.name,
-            _index: PhantomData,
+            ptr:  null(),
+            size:  0,
+            _lt: PhantomData,
         }
     }
 }
 
+struct DTIWorkingNode<'dt, 'i: 'dt> {
+    parent: *mut DTIWorkingNode<'dt, 'i>,
+    children: GrowableSlice<'i, DTINode<'dt, 'i>>,
+    props: GrowableSlice<'i, DTIProp<'dt>>,
+    name: &'dt [u8],
+}
+
+impl<'dt, 'i: 'dt> DTIWorkingNode<'dt, 'i> {
+}
+
+
 pub struct DevTreeIndex<'dt, 'i: 'dt> {
     fdt: &'i DevTree<'dt>,
-    root: Box<DTINode<'dt, 'i>>,
+    buf: &'i mut [u8],
+    // TODO root
+}
+
+struct DevTreeIndexBuildState<'dt, 'i: 'dt> {
+    buf: &'i mut [u8],
+    cur_node: *mut DTIWorkingNode<'dt, 'i>,
+    front_off: usize,
+    back_off: usize,
+}
+
+impl<'dt, 'i:'dt> DevTreeIndexBuildState<'dt, 'i> {
+    pub unsafe fn insert_node(&mut self, node: &iters::ParsedBeginNode<'dt>) {
+        // Align pointer
+        let ptr = (self.front_off as *mut u8).align_offset(align_of::<DTIWorkingNode>()) as *mut DTIWorkingNode;
+        assert!(ptr_in(self.buf, ptr));
+
+        // Write the data
+        *ptr = DTIWorkingNode {
+            parent: self.cur_node,
+            children: GrowableSlice::new(),
+            props: GrowableSlice::new(),
+            name: node.name,
+        };
+        // Save the new node ptr
+        self.cur_node = ptr;
+        // Increment offset
+        self.front_off = ptr.add(1) as usize;
+
+        assert!(self.front_off <= self.back_off);
+    }
+
+    pub unsafe fn append_prop(&mut self, prop: &iters::ParsedProp<'dt>) {
+        // Align pointer
+        let ptr = (self.front_off as *mut u8).align_offset(align_of::<DTIProp>()) as *mut DTIProp;
+        assert!(ptr_in(self.buf, ptr));
+        // Write the data
+        *ptr = DTIProp::from(prop);
+        // Increment offset
+        self.front_off = ptr.add(1) as usize;
+
+        assert!(self.front_off <= self.back_off);
+    }
 }
 
 impl<'dt, 'i: 'dt> DevTreeIndex<'dt, 'i> {
-    fn get_root_node(
-        iter: &mut iters::DevTreeParseIter<'dt>,
-    ) -> Result<Box<DTINode<'dt, 'i>>, DevTreeError> {
-        // Prime the initial current_node
+    unsafe fn init_state(&'i mut self, iter: &mut iters::DevTreeParseIter<'dt>) -> Result<DevTreeIndexBuildState<'dt, 'i>, DevTreeError> {
+        let mut state = DevTreeIndexBuildState {
+            front_off: 0,
+            back_off: self.buf.len() -1,
+            buf: self.buf,
+            cur_node: null_mut(),
+        };
+
         for tok in iter {
             match tok {
                 iters::ParsedTok::BeginNode(node) => {
-                    return Ok(Box::new(DTINode::new(None, node)))
-                }
-                iters::ParsedTok::Nop => (),
+                    state.insert_node(&node);
+                    return Ok(state);
+                },
+                iters::ParsedTok::Nop => continue,
                 _ => return Err(DevTreeError::ParseError),
             }
         }
         Err(DevTreeError::ParseError)
     }
 
-    pub fn new(fdt: &'i DevTree<'dt>) -> Result<Self, DevTreeError> {
-        let mut iter = iters::DevTreeParseIter::new(fdt);
+    // Note: This parsing method is particularly unsafe. We decide this is worth it because it
+    // enables running this method without any allocator and uses minimal overhead.
+    pub unsafe fn new(fdt: &'i DevTree<'dt>, buf: &'i mut [u8]) -> Result<Self, DevTreeError> {
+        let mut this = Self {
+            fdt, buf,
+        };
+        let mut iter = iters::DevTreeParseIter::new(this.fdt);
 
-        let mut root = Self::get_root_node(&mut iter)?;
-        let mut cur_node = root.as_mut();
+        let mut state = this.init_state(&mut iter)?;
 
         // Devtree Props may only occur before child nodes. We'll call this the "node_header".
         let mut in_node_header = true;
 
+        // The buffer will be split into two parts, front and back:
+        //
+        // Front will be used as a temporary work section to  build the nodes as we parse them.
+        // The back will be used to save completely parsed nodes.
         for tok in iter {
             match tok {
                 iters::ParsedTok::BeginNode(node) => {
-                    // Allocate node from parsed node.
-                    // FIXME: Casting the reference to a pointer results in a dangling pointer in
-                    // the event that the Vec reallocates.
-                    cur_node
-                        .children
-                        .push(DTINode::new(Some(cur_node), node));
-                    // (Unwrap safe, we just pushed a node.)
-                    unsafe {
-                        cur_node = cur_node.children.last_mut().unsafe_unwrap();
-                    }
+                    state.insert_node(&node);
+                    in_node_header = true;
                 }
                 iters::ParsedTok::Prop(prop) => {
                     if !in_node_header {
                         return Err(DevTreeError::ParseError);
                     }
-                    cur_node.props.push(DTIProp::from(prop));
+
+                    // Push the prop
+                    state.append_prop(&prop)
                 }
                 iters::ParsedTok::EndNode => {
-                    // Cast the current node's *const parent pointer into a mutable reference.
-                    //
-                    // This is safe because this will be the only mutable reference to the parent
-                    // while this function is active.
-                    //
-                    // We believe this to not violate the aliasing rules as they are currently
-                    // defined. This soon to be mutable reference is the only way we access parent
-                    // while the mutable reference exists.
-                    //
-                    // Quote: https://doc.rust-lang.org/std/cell/struct.UnsafeCell.html
-                    //
-                    // The precise Rust aliasing rules are somewhat in flux, but the main points
-                    // are not contentious:
-                    //
-                    //  -  If you create a safe reference with lifetime 'a (either a &T or &mut
-                    //     T reference) that is accessible by safe code (for example, because you
-                    //     returned it), then you must not access the data in any way that
-                    //     contradicts that reference for the remainder of 'a. For example, this
-                    //     means that if you take the *mut T from an UnsafeCell<T> and cast it to
-                    //     an &T, then the data
-                    //     in T must remain immutable (modulo any UnsafeCell data found within T,
-                    //     of course) until that reference's lifetime expires. Similarly, if you
-                    //     create a &mut T reference that is released to safe code, then you must
-                    //     not access the data within the UnsafeCell until that reference expires.
-                    unsafe {
-                        let n_ref = cur_node.parent.ok_or(DevTreeError::ParseError)?;
-                        cur_node = &mut *(n_ref as *mut DTINode<'dt, 'i>);
-                    }
+                    // TODO We've completely parsed cur_node (both props and node).
+                    // Let's move the node into perminent section in the back.
                     in_node_header = false;
+
+                    // TODO Fixup all parent links in our children.
                 }
                 iters::ParsedTok::Nop => continue,
             }
         }
 
-        Ok(Self { root, fdt })
+        todo!()
     }
 }
 
-//pub struct DevTreeIndexNode<'dt, 'i: 'dt> {
-//    pub index: &'i DevTreeIndex<'dt, 'i>,
-//    node: &'i DTINode<'dt, 'i>,
+////pub struct DevTreeIndexNode<'dt, 'i: 'dt> {
+////    pub index: &'i DevTreeIndex<'dt, 'i>,
+////    node: &'i DTINode<'dt, 'i>,
+////}
+////
+////impl<'dt, 'i: 'dt> DevTreeIndexNode<'dt, 'i> {
+////    fn new(node: &'i DTINode<'dt, 'i>, index: DevTreeIndex<'dt, 'i>) -> Self {
+////        Self {
+////            node,
+////            index
+////        }
+////    }
+////}
+//#[derive(Clone)]
+//pub struct DevTreeIndexNode<'dt, 'i: 'dt, 'a: 'i> {
+//    pub index: &'a DevTreeIndex<'dt, 'i>,
+//    node: &'a DTINode<'dt, 'i>,
 //}
 //
-//impl<'dt, 'i: 'dt> DevTreeIndexNode<'dt, 'i> {
-//    fn new(node: &'i DTINode<'dt, 'i>, index: DevTreeIndex<'dt, 'i>) -> Self {
+//impl<'dt, 'i: 'dt, 'a: 'i> DevTreeIndexNode<'dt, 'i, 'a> {
+//    fn new(node: &'a DTINode<'dt, 'i>, index: &'a DevTreeIndex<'dt, 'i>) -> Self {
 //        Self {
 //            node,
 //            index
 //        }
 //    }
 //}
-#[derive(Clone)]
-pub struct DevTreeIndexNode<'dt, 'i: 'dt, 'a: 'i> {
-    pub index: &'a DevTreeIndex<'dt, 'i>,
-    node: &'a DTINode<'dt, 'i>,
-}
-
-impl<'dt, 'i: 'dt, 'a: 'i> DevTreeIndexNode<'dt, 'i, 'a> {
-    fn new(node: &'a DTINode<'dt, 'i>, index: &'a DevTreeIndex<'dt, 'i>) -> Self {
-        Self {
-            node,
-            index
-        }
-    }
-}
-
-// TODO Note iterator
-/// An interator over [`DevTreeNode`] objects in the [`DevTree`]
-#[derive(Clone)]
-pub struct DevTreeIndexNodeIter<'dt, 'i: 'dt, 'a: 'i> {
-    index_node: DevTreeIndexNode<'dt, 'i, 'a>
-}
-
-impl<'dt, 'i: 'dt, 'a: 'i> DevTreeIndexNodeIter<'dt ,'i, 'a> {
-    pub(crate) fn new(index: &'a DevTreeIndex<'dt, 'i>) -> Self {
-        Self {
-            index_node: DevTreeIndexNode::new(index.root.as_ref(), index)
-        }
-    }
-
-    // See the documentation of [`DevTree::find_node`]
-    //#[inline]
-    //pub fn find<F>(&mut self, predicate: F) -> Option<(DevTreeNode<'a>, Self)>
-    //where
-    //    F: Fn(&DevTreeNode) -> Result<bool, DevTreeError>,
-    //{
-    //}
-}
-
-impl<'dt, 'i: 'dt, 'a: 'i> Iterator for DevTreeIndexNodeIter<'dt ,'i, 'a> {
-    type Item = DevTreeIndexNode<'dt ,'i, 'a>;
-    fn next(&mut self) -> Option<Self::Item> {
-        todo!()
-    }
-}
+//
+//// TODO Note iterator
+///// An interator over [`DevTreeNode`] objects in the [`DevTree`]
+//#[derive(Clone)]
+//pub struct DevTreeIndexNodeIter<'dt, 'i: 'dt, 'a: 'i> {
+//    index_node: DevTreeIndexNode<'dt, 'i, 'a>
+//}
+//
+//impl<'dt, 'i: 'dt, 'a: 'i> DevTreeIndexNodeIter<'dt ,'i, 'a> {
+//    pub(crate) fn new(index: &'a DevTreeIndex<'dt, 'i>) -> Self {
+//        Self {
+//            index_node: DevTreeIndexNode::new(index.root.as_ref(), index)
+//        }
+//    }
+//
+//    // See the documentation of [`DevTree::find_node`]
+//    //#[inline]
+//    //pub fn find<F>(&mut self, predicate: F) -> Option<(DevTreeNode<'a>, Self)>
+//    //where
+//    //    F: Fn(&DevTreeNode) -> Result<bool, DevTreeError>,
+//    //{
+//    //}
+//}
+//
+//impl<'dt, 'i: 'dt, 'a: 'i> Iterator for DevTreeIndexNodeIter<'dt ,'i, 'a> {
+//    type Item = DevTreeIndexNode<'dt ,'i, 'a>;
+//    fn next(&mut self) -> Option<Self::Item> {
+//        todo!()
+//    }
+//}
 
 // TODO Fuck load of utility methods.
